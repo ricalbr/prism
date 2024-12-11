9504fd44df001665292cdeae4f58dd4040aceb1e
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2024-12-11 20:49:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
+import scipy.stats as sst
 import prism
 from prism import (
     EMEsolver,
@@ -22,13 +23,13 @@ def main():
     iterations = int(1e5)
 
     # INPUT PHOTON STATISTICS
-    ph_stat = scipy.stats.rv_discrete(values=([20], [1]))
-    ph_stat = scipy.stats.rv_discrete(values=([2], [1]))
-    ph_stat = scipy.stats.rv_discrete(values=([1, 3], [0.5, 0.5]))
-    ph_stat = scipy.stats.poisson(mu=40)
-    # ph_stat = scipy.stats.poisson(mu=2)
-    # ph_stat = scipy.stats.boltzmann(lambda_=0.2, N=6)
-    # ph_stat = scipy.stats.rv_discrete(values=([1, 3, 5, 7, 9, 11, 13], np.ones((7,))/7))
+    ph_stat = sst.rv_discrete(values=([20], [1]))
+    ph_stat = sst.rv_discrete(values=([2], [1]))
+    ph_stat = sst.rv_discrete(values=([1, 3], [0.5, 0.5]))
+    ph_stat = sst.poisson(mu=40)
+    # ph_stat = sst.poisson(mu=2)
+    # ph_stat = sst.boltzmann(lambda_=0.2, N=6)
+    # ph_stat = sst.rv_discrete(values=([1, 3, 5, 7, 9, 11, 13], np.ones((7,))/7))
 
     # GET SPAD MATRIX AND CLICKS ARRAY
     V = get_spad_matrix(num_det=D, eta=eta, dcr=mean_dcr, xtk=xtk)
@@ -38,7 +39,7 @@ def main():
     # c = np.loadtxt('click_vector')
 
     # SOLVE THE c = Vp_n PROBLEM
-    p_n = EMEsolver(V, c, alpha=1e-2, iterations=1e10, epsilon=1e-18)
+    p_n = EMEsolver(V, c, alpha=1e-2, epsilon=1e-18)
 
     # FIDELITY
     p_space = np.array(range(D + 1))
@@ -58,7 +59,6 @@ def main():
     plt.title(f"Reconstructed p vector")
     plt.xlabel("n")
     plt.ylabel("p(n)")
-    plt.ylim([0, 1])
     plt.show()
 
 
