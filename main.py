3b56dff8652a1b327eb53a4d02026a82530f0ff3
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2024-12-12 17:01:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,42 +4,37 @@ from __future__ import annotations
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as sst
-from prism import (
-    EMEsolver,
-    get_dcr_array,
-    get_spad_matrix,
-    get_clicks_array,
-)
-
+import prism as pp
 
 def main():
     # SPAD ARRAY/MATRIX PROPERTIES
     D = 64
     eta = 0.80
-    mean_dcr, dcr = get_dcr_array(num_det=D, dcr_min=1e-3, dcr_max=1e-2)
+    mean_dcr, dcr = pp.get_dcr_array(num_det=D, dcr_min=1e-3, dcr_max=1e-2)
     xtk = 0.05
     iterations = int(1e5)
 
     # INPUT PHOTON STATISTICS
     ph_stat = sst.rv_discrete(values=([20], [1]))
-    ph_stat = sst.rv_discrete(values=([2], [1]))
-    ph_stat = sst.rv_discrete(values=([1, 3], [0.5, 0.5]))
-    ph_stat = sst.poisson(mu=40)
+    # ph_stat = sst.rv_discrete(values=([2], [1]))
+    # ph_stat = sst.rv_discrete(values=([1, 3], [0.5, 0.5]))
+    # ph_stat = sst.poisson(mu=40)
     # ph_stat = sst.poisson(mu=2)
     # ph_stat = sst.boltzmann(lambda_=0.2, num_ph=6)
     # ph_stat = sst.rv_discrete(values=([1, 3, 5, 7, 9, 11, 13], np.ones((7,))/7))
 
     # GET SPAD MATRIX AND CLICKS ARRAY
-    V = get_spad_matrix(num_det=D, eta=eta, dcr=mean_dcr, xtk=xtk)
-    c = get_clicks_array(num_det=D, eta=eta, dcr=dcr, xtk=xtk, ph_stat=ph_stat, it=iterations)
+    V = pp.get_spad_matrix(num_det=D, eta=eta, dcr=mean_dcr, xtk=xtk)
+    c = pp.get_clicks_array(num_det=D, eta=eta, dcr=dcr, xtk=xtk, ph_stat=ph_stat, it=iterations)
+    # np.savetxt('c_vec', c)
+    # c = np.loadtxt('c_vec')
 
     # SOLVE THE c = Vp_n PROBLEM
-    p_n = EMEsolver(V, c, alpha=1e-2, epsilon=1e-18)
+    p_n = pp.EMEsolver(V, c, alpha=1e0, epsilon=1e-18, max_stagnation=1e6)
 
     # FIDELITY
     p_space = np.array(range(D + 1))
-    fid = 1 - np.linalg.norm(p_n - ph_stat.pmf(p_space))
-    print(f"Fidelity of the reconstruction: {fid:.3f}.")
+    print(f"Fidelity of the reconstruction: {pp.fidelity(p_n, ph_stat.pmf(p_space)):.3f}.")
 
     # PREPARE PLOT
     plt.figure()
