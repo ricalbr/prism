c88ff31de776a93f7e1c2f903f3517928d29c7a9
ricalbr <albr.riccardo@gmail.com>
2025-02-05 19:01:16 +0100

    Update make_string function

--- include/prism/matrix.hpp
+++ include/prism/matrix.hpp
@@ -2,14 +2,14 @@
 #define PRISM_MATRIX_HPP
 
 #include <Eigen/Dense>
-#include <string>
 
 long long comb(int n, int k);
 
-std::string make_key(int D, double eta, double dcr, int N, int C, int K);
+template <typename... Args> std::string make_key(Args... args);
 
 double analytic(int D, double eta, double dcr, int N, int L);
 
-void get_spad_matrix(Eigen::MatrixXd &A, int num_det, double eta, double dcr, double xtk);
+void get_spad_matrix(Eigen::MatrixXd &A, int num_det, double eta, double dcr,
+                     double xtk);
 
 #endif // PRISM_MATRIX_HPP
