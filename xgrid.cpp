99af95afdc9984718fd9d33e8cf7ba6ace5d226d
ricalbr <albr.riccardo@gmail.com>
2025-02-12 23:55:36 +0100

    Fix code and improve performances

--- xgrid.cpp
+++ xgrid.cpp
@@ -19,50 +19,56 @@ vector<vector<bool>> set_init_matrix(int num) {
 
 int32_t main(int argc, char *argv[]) {
 
-    int num = 10;
+    int num = 10000;
+    auto start_time = std::chrono::high_resolution_clock::now();
     vector<vector<bool>> grid = set_init_matrix(num);
     int row = grid.size();
     int col = grid[0].size();
 
     auto is_valid = [&](int a, int b) {
-        return (0 <= a) && (a < row) && (0 <= b) && (b <= col);
+        return (0 <= a) && (a < row) && (0 <= b) && (b < col);
     };
 
     queue<pair<int, int>> nn, xt;
     vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            for (pair<int, int> dir : directions) {
-                int coordy = i + dir.first;
-                int coordx = j + dir.second;
-                if (is_valid(coordy, coordx) && grid[i][j] &&
-                    !grid[coordx][coordy]) {
-                    nn.push({coordy, coordx});
+
+    for (int y = 0; y < row; y++) {
+        for (int x = 0; x < col; x++) {
+            if (!grid[y][x])
+                continue;
+            for (auto &[dy, dx] : directions) {
+                int ny = y + dy, nx = x + dx;
+                if (is_valid(ny, nx) && !grid[ny][nx]) {
+                    nn.push({ny, nx});
                 }
             }
         }
     }
     do {
         while (!xt.empty()) {
-            pair<int, int> coord = xt.front();
+            auto [y, x] = xt.front();
             xt.pop();
-            for (pair<int, int> dir : directions) {
-                int coordy = coord.first + dir.first;
-                int coordx = coord.second + dir.second;
-                if (is_valid(coordy, coordx) &&
-                    grid[coord.first][coord.second] && !grid[coordy][coordx]) {
-                    nn.push({coordx, coordy});
+            if (grid[y][x])
+                continue;
+            for (auto &[dy, dx] : directions) {
+                int ny = y + dy, nx = x + dx;
+                if (is_valid(ny, nx) && !grid[ny][nx]) {
+                    nn.push({ny, nx});
                 }
             }
         }
         while (!nn.empty()) {
-            if ((double)rand() / RAND_MAX) {
+            if ((double)rand() / RAND_MAX < XTALK_PROB) {
                 xt.push(nn.front());
                 grid[nn.front().first][nn.front().second] = true;
             }
             nn.pop();
         }
     } while (!xt.empty());
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end_time - start_time;
+    std::cout << "Elapsed time: " << elapsed_seconds.count() << " s\n";
 
     // for (int i = 0; i < num; i++) {
     //     for (int j = 0; j < num; j++) {
