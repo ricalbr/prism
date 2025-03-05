44d2b3d19608dd21469ece609d7514aafb187718
ricalbr <albr.riccardo@gmail.com>
2025-03-05 12:21:06 +0100

    Add build folder

---
+++ build/CMakeFiles/prism.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prism.dir/src/config.cpp.o"
+  "CMakeFiles/prism.dir/src/matrix.cpp.o"
+  "CMakeFiles/prism.dir/src/random.cpp.o"
+  "CMakeFiles/prism.dir/src/simulation.cpp.o"
+  "CMakeFiles/prism.dir/src/solver.cpp.o"
+  "CMakeFiles/prism.dir/src/utils.cpp.o"
+  "CMakeFiles/prism.dir/src/xtalk.cpp.o"
+  "lib/libprism.a"
+  "lib/libprism.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prism.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
