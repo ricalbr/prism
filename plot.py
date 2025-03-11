a4fd43f1ad8d9ef272ff80a95ed1d2a6e0397797
ricalbr <albr.riccardo@gmail.com>
2025-03-11 10:17:47 +0100

    Update plots

--- plot.py
+++ plot.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import pathlib
 
 import matplotlib.pyplot as plt
+from matplotlib import rc
+
+rc('font', **{'family': 'sans-serif', 'sans-serif': ['Nimbus Sans'], 'size': 14})
 import numpy as np
 import scipy.stats as stats
 import yaml
@@ -33,7 +36,10 @@ def get_fidelity(filename, distribution, cwd, plot=False):
     p_n = np.loadtxt(filename)
     p_space = np.array(range(len(p_n)))
     p_stat = distribution.pmf(p_space)
-    fid = 1 - np.linalg.norm(p_n - p_stat)
+    # fid = 1 - np.linalg.norm(p_n - p_stat)
+    # print(f'{fid=}')
+    fid = np.sum([np.sqrt(p_n * p_stat)]) ** 2
+    print(f'{fid=}')
 
     # EXPORT
     np.savetxt(cwd / 'predicted.txt', p_n)
@@ -41,15 +47,17 @@ def get_fidelity(filename, distribution, cwd, plot=False):
 
     # PLOT
     plt.figure(1)
-    plt.bar(p_space, p_n, alpha=0.6, color="red", label="Analytic + EME")
-    plt.plot(p_stat, "o--k", label="Theoretical p(n)")
+    plt.bar(p_space, p_n, alpha=0.4, color="blue", label="Retrieved distribution")
+    plt.plot(p_stat, "ok", label="Theoretical distribution")
 
     plt.legend()
     plt.xlabel("n")
     plt.ylabel("p(n)")
+    plt.title(f'Reconstruction fidelity: {fid*100:.4f}%')
     # plt.ylim([0, 1])
-    # if plot:
-    plt.show()
+
+    if plot:
+        plt.show()
     plt.savefig(cwd / 'plot.png')
     return fid
 
@@ -60,10 +68,13 @@ def main():
     yaml_data = load_yaml("config.yaml")
     photon_distribution = yaml_data.get("photon_distribution", {})
     distribution = create_distribution(photon_distribution)
-    try:
-        filename = f'N{yaml_data.get("rows")}x{yaml_data.get("cols")}_DE{yaml_data.get("eta")}_XT{yaml_data.get("xtk")}_STAT_{photon_distribution.get("type")}_{photon_distribution.get("mean")}'
-    except:
-        filename = f'N{yaml_data.get("rows")}x{yaml_data.get("cols")}_DE{yaml_data.get("eta")}_XT{yaml_data.get("xtk")}_STAT_{photon_distribution.get("type")}_{photon_distribution.get("probabilities")}'
+    r, c, e, x = yaml_data.get("num_row"), yaml_data.get("num_col"), yaml_data.get("eta"), yaml_data.get("xtk")
+    filename = f'M{r}x{c}_PDE{e*100}_XT{x*100}'
+    if photon_distribution.get('type') == 'poisson':
+        filename += f'_STAT_{photon_distribution.get("type")}_{photon_distribution.get("mean")}'
+    else:
+        stat = '-'.join([str(p) for p in photon_distribution.get("probabilities")])
+        filename += f'_STAT_{photon_distribution.get("type")}_{stat}'
 
     cwd = pathlib.Path('.') / 'simulations' / filename
     pathlib.Path(cwd).mkdir(parents=True, exist_ok=True)
