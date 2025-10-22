15ab0e0dad136a62666133b08c736fe676fb86c5
ricalbr <albr.riccardo@gmail.com>
2025-10-22 16:37:20 +0200

    Reformat code

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -16,8 +16,8 @@ struct hash_pair {
 
 using namespace Eigen;
 
-int simulate(int rows, int cols, int num_ph, double eta, const Eigen::VectorXd &dcr, double xtk,
-             Xoshiro256PlusPlus &rng) {
+int simulate(int rows, int cols, int num_ph, double eta,
+             const Eigen::VectorXd &dcr, double xtk, Xoshiro256PlusPlus &rng) {
     std::unordered_set<std::pair<int, int>, hash_pair> detector_mat;
     std::queue<std::pair<int, int>> clicked;
 
@@ -36,7 +36,8 @@ int simulate(int rows, int cols, int num_ph, double eta, const Eigen::VectorXd &
         if (rng.next_double() < eta) {
             int r = rng.next() % rows, c = rng.next() % cols;
             if (detector_mat.insert({r, c})
-                    .second) { // .second is true only if the element is inserted in the set for the first time
+                    .second) { // .second is true only if the element is
+                               // inserted in the set (for the first time)
                 clicked.push({r, c});
             }
         }
@@ -44,12 +45,14 @@ int simulate(int rows, int cols, int num_ph, double eta, const Eigen::VectorXd &
 
     // Crosstalk
     if (xtk > 0.0) {
-        static const std::vector<std::pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        static const std::vector<std::pair<int, int>> directions = {
+            {-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        auto is_valid = [&](int a, int b) { return (0 <= a) && (a < rows) && (0 <= b) && (b < cols); };
+        auto is_valid = [&](int a, int b) {
+            return (0 <= a) && (a < rows) && (0 <= b) && (b < cols);
+        };
 
         std::queue<std::pair<int, int>> nn;
-
         do {
             while (!clicked.empty()) {
                 // current clicked pixel
@@ -59,7 +62,8 @@ int simulate(int rows, int cols, int num_ph, double eta, const Eigen::VectorXd &
                 // find nearest neighbors
                 for (const auto &[drow, dcol] : directions) {
                     int nr = r + drow, nc = c + dcol;
-                    if (is_valid(nr, nc) && detector_mat.find({nr, nc}) == detector_mat.end()) {
+                    if (is_valid(nr, nc) &&
+                        detector_mat.find({nr, nc}) == detector_mat.end()) {
                         nn.push({nr, nc});
                     }
                 }
@@ -69,6 +73,7 @@ int simulate(int rows, int cols, int num_ph, double eta, const Eigen::VectorXd &
             while (!nn.empty()) {
                 auto [nr, nc] = nn.front();
                 nn.pop();
+
                 if (rng.next_double() < xtk) {
                     if (detector_mat.insert({nr, nc}).second) {
                         clicked.push({nr, nc});
@@ -79,8 +84,11 @@ int simulate(int rows, int cols, int num_ph, double eta, const Eigen::VectorXd &
     }
     return detector_mat.size();
 }
-VectorXd get_clicks_array(int rows, int cols, double eta, const Eigen::VectorXd &dcr, double xtk, int iterations,
-                          const std::function<int(Xoshiro256PlusPlus &)> &photon_distribution, uint64_t seed) {
+VectorXd get_clicks_array(
+    int rows, int cols, double eta, const Eigen::VectorXd &dcr, double xtk,
+    int iterations,
+    const std::function<int(Xoshiro256PlusPlus &)> &photon_distribution,
+    uint64_t seed) {
 
     int num_det = rows * cols;
     VectorXd frequencies = VectorXd::Zero(num_det + 1);
@@ -97,7 +105,8 @@ VectorXd get_clicks_array(int rows, int cols, double eta, const Eigen::VectorXd 
             if (sum_clicks < num_det) { // Ensure valid index
                 local_frequencies[sum_clicks] += 1.0;
             } else {
-                std::cout << "Saturating... measured clicks: " << sum_clicks << "\n";
+                std::cout << "Saturating... measured clicks: " << sum_clicks
+                          << "\n";
                 local_frequencies[local_frequencies.size() - 1] += 1.0;
             }
         }
